5a7c4da90296c3362b2614b18f789ad58209fd0c
Walex101019 <ibietanwasiu94@gmail.com>
2024-01-12 01:48:13 -0800

    simple-shell

---
+++ commands.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+/*
+ * execute_command - Entry point
+ * @command: command passed
+ */
+void execute_command(const char *command)
+{
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+	}
+	else if (pid == 0)
+	{
+		execlp(command, command, NULL);
+		perror("exec");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			if (WEXITSTATUS(status) != 0)
+			{
+				fprintf(stderr, "Error: Command '%s' failed with exit code %d\n", command, WEXITSTATUS(status));
+			}
+		}
+		else
+		{
+			fprintf(stderr, "Error: Command '%s' did not terminate successfully\n", command);
+		}
+	}
+}
---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+
+#endif /* SHELL_H */
